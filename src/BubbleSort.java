d9cfd55d93bf4bcf1e96a9caf5819cda4514b5ed
chenls520 <406797103@qq.com>
2019-01-28 21:25:17 +0800

    冒泡排序

---
+++ src/BubbleSort.java
@@ -0,0 +1,31 @@
+/**
+ * 冒泡排序
+ * @author Administrator
+ *
+ *冒泡排序的基本思想是：每次比较两个相邻的元素，如果他们的顺序错误就把他们交换过来
+ *每次循环只能将一个数字归为，即n个数进行排序只需要进行n-1次。
+ */
+public class BubbleSort {
+	
+	public static void main(String[] args) {
+		PrintUtil.print(new BubbleSort().sort(PrintUtil.ARRAY));
+	}
+
+	
+	private int[] sort(int[] params) {
+		//循环n-1次
+		for(int i = 1; i < params.length; i++) {
+			//两两进行比较，循环n-1次
+			for(int j = 0; j < params.length - 1; j ++) {
+				//前面的小于后面进行交换则为升序排列，反之降序
+				if (params[j] < params[j + 1]) {
+					int temp = params[j];
+					params[j] = params[j + 1];
+					params[j + 1] = temp;
+				}
+			}
+		}
+		
+		return params;
+	}
+}
---
+++ src/PrintUtil.java
@@ -0,0 +1,18 @@
+/**
+ * ¥Ú”°π§æﬂ
+ * @author Administrator
+ *
+ */
+public class PrintUtil {
+	
+	public static int[] ARRAY = {1, 8, 6, 5, 6, 5, 5};
+	
+	public static void print(int[] params) {
+		for(int i = 0; i < params.length; i++) {
+			System.out.println(params[i]);
+		}
+		
+	}
+	
+	
+}
